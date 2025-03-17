f830c087cd57fa620de836efa0ff0bce1fbd3f04
Divya Dattatray Sakhare <143021718+Divyasakhare07@users.noreply.github.com>
2025-03-17 11:57:47 -0400

    Time: 603 ms (87.93%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,7 +1,12 @@
-SELECT name
+/*SELECT name
 FROM Customer
 WHERE id NOT IN(
     SELECT id 
     FROM Customer
     where referee_id = 2
 )
+*/
+
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2;
