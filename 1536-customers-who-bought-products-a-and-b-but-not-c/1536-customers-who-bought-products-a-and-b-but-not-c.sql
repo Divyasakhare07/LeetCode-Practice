294db437ebef7668b3945254f0aeee74226584ac
Divya Dattatray Sakhare <143021718+Divyasakhare07@users.noreply.github.com>
2025-04-22 15:48:26 -0400

    Time: 690 ms (67.55%), Space: 0B (100%) - LeetHub

---
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/1536-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select distinct(O.customer_id) as customer_id , C. customer_name 
+from Orders O join Customers C
+on O.customer_id = C.customer_id 
+where O.customer_id in (select Customer_id from Orders where product_name = 'A') 
+and O.customer_id in (select Customer_id from Orders where product_name = 'B')
+and O.customer_id  not in (select Customer_id from Orders where product_name = 'C')
