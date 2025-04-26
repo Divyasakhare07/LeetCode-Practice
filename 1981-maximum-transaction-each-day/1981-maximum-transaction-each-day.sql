1392fb160daf9b40e908d48a359179508a6b0536
Divya Dattatray Sakhare <143021718+Divyasakhare07@users.noreply.github.com>
2025-04-26 18:04:56 -0400

    Time: 537 ms (81.58%), Space: 0B (100%) - LeetHub

--- 1981-maximum-transaction-each-day/1981-maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/1981-maximum-transaction-each-day.sql
@@ -1,7 +1,19 @@
 # Write your MySQL query statement below
-select transaction_id 
+
+# using max function
+/*select transaction_id 
 from Transactions
 where (day, amount) in (
 select day, max(amount) over (partition by day order by day) as maxamount
 from Transactions)
+order by 1*/
+
+with cte as
+(select day, amount, rank() over (partition by day order by amount desc) as rrank
+from Transactions)
+
+select transaction_id 
+from Transactions
+where (day, amount) in( select day, amount from cte where rrank = 1)
+
 order by 1
